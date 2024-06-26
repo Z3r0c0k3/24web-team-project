20a3853208bd558ccc4434f723050c2a012c7741
Z3r0c0k3 <hackerj1128@gmail.com>
2024-06-26 17:44:25 +0900

    modify: script.js

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ document.addEventListener('DOMContentLoaded', () => { // 문서 로드 후 실�
       const now = new Date().getTime();
       const timeDifference = targetDate - now;
 
-      if (timeDifference <= 0) {
-        console.log(`${className}: D-DAY!`);
-      }
-
         const days = Math.max(0, Math.floor(timeDifference / (1000 * 60 * 60 * 24)));
         const hours = Math.max(0, Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const minutes = Math.max(0, Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)));
@@ -36,4 +32,24 @@ document.addEventListener('DOMContentLoaded', () => { // 문서 로드 후 실�
     updateDday(className, targetDates[className]);
     setInterval(() => updateDday(className, targetDates[className]), 1);
   }
+    
+  function dateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 1을 더함
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+  
+    const formattedDate = `${year}. ${month}. ${day}`;
+    const formattedTime = `${ampm} ${hours % 12 === 0 ? 12 : hours % 12} : ${minutes} : ${seconds}`;
+  
+    document.querySelector('.date').innerText = formattedDate;
+    document.querySelector('.time').innerText = formattedTime;
+}
+
+dateTime();
+setInterval(dateTime, 500);
 });
